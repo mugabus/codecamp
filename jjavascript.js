4b7591783f39f1dee07f4e23abfb5b439030a859
mugabus <fatakimugaruka91@gmail.com>
2024-08-22 10:57:12 +0200

    how to declar valariable

---
+++ jjavascript.js
@@ -0,0 +1,8 @@
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+console.log(rows);
+let popped;
+rows.pop(popped)
+console.log(popped)
