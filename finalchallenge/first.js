94fac459a6c8239f9ef68da3e4c17a1870319d5d
mugabus <fatakimugaruka91@gmail.com>
2024-08-29 21:52:30 +0200

    update

--- finalchallenge/first.js
+++ finalchallenge/first.js
@@ -2,13 +2,12 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow() {
-
+function padRow(rowNumber, rowCount) {
+  return character.repeat(rowNumber);
 }
 
-
 for (let i = 0; i < count; i = i + 1) {
-  rows.push(character.repeat(i + 1))
+  rows.push(padRow(i + 1, count));
 }
 
 let result = ""
