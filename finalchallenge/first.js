d4e33760c8e3caaba8f35778a4e58a97d066f187
mugabus <fatakimugaruka91@gmail.com>
2024-08-29 22:54:03 +0200

    code updated

--- finalchallenge/first.js
+++ finalchallenge/first.js
@@ -6,9 +6,20 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-
-for (let i = 1; i <= count; i++) {
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count));
+}*/
+
+let continueLoop = false;
+let done = 0;
+
+while (continueLoop) {
+  done++;
+  rows.push(padRow(done, count));
+  if (done === count) {
+    continueLoop = false;
+  } 
 }
 
 let result = ""
