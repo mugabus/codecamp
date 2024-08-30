bca2eec8d0bbc451878d18a5ec36d4707ea93f02
mugabus <fatakimugaruka91@gmail.com>
2024-08-30 09:27:41 +0200

    update

--- finalchallenge/first.js
+++ finalchallenge/first.js
@@ -11,8 +11,12 @@ function padRow(rowNumber, rowCount) {
   rows.push(padRow(i, count));
 }*/
 
-while (rows.length < count) {
+/*while (rows.length < count) {
   rows.push(padRow(rows.length + 1, count));
+}*/
+
+for (let i = count; i > 0; false) {
+
 }
 
 let result = ""
