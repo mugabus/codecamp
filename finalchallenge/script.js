681d0b87a0c287f2dd2ad3a9b6e0b1793ced43c0
mugabus <fatakimugaruka91@gmail.com>
2024-08-30 12:08:35 +0200

    update

--- finalchallenge/script.js
+++ finalchallenge/script.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeaponIndex = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+const button1 = document.querySelector("#button1");
