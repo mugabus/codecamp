c3c437e14aa6755601908e947010e8904807ca68
mugabus <fatakimugaruka91@gmail.com>
2024-08-30 12:53:22 +0200

    creating function

--- finalchallenge/script.js
+++ finalchallenge/script.js
@@ -17,10 +17,16 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+// initialize buttons
+
 function goStore() {
   console.log("Going to store.");
 }
 
-function goCave(){
+function goCave() {
   console.log("Going to cave.");
+}
+
+function fightDragon() {
+  console.log("Fighting dragon.");
 }
