3abec21365c6110b2f804576c88231cc29bafc48
mugabus <fatakimugaruka91@gmail.com>
2024-08-31 21:32:26 +0200

    isMonsterHit function added

--- finalchallenge/script.js
+++ finalchallenge/script.js
@@ -181,8 +181,12 @@ function goFight() {
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
-  health -= monsters[fighting].level;
-  monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  health -= getMonsterAttackValue(monsters[fighting].level);
+  if (isMonsterHit()) {
+    monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;    
+  } else {
+    text.innerText += " You miss.";
+  }
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
   if (health <= 0) {
@@ -193,6 +197,18 @@ function attack() {
     } else {
       defeatMonster();
     }
+  }
+}
+
+function getMonsterAttackValue(level) {
+  const hit = (level * 5) - (Math.floor(Math.random() * xp));
+  console.log(hit);
+  return hit > 0 ? hit : 0;
+}
+
+function isMonsterHit(){
+  if(true){
+    return Math.random() > .2;
   }
 }
 
