62cbdc7db81a49b4abd048e48e91dd510372fbff
mugabus <fatakimugaruka91@gmail.com>
2024-08-31 20:50:23 +0200

    start with the lose function

--- finalchallenge/script.js
+++ finalchallenge/script.js
@@ -68,8 +68,8 @@ const locations = [
     name: "kill monster",
     "button text": ["Go to town square", "Go to town square", "Go to town square"],
     "button functions": [goTown, goTown, goTown],
-    text: "The monster screams Arg! as it dies. You gain experience points and find gold."
-  }
+    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+  },
 ];
 
 // initialize buttons
@@ -78,6 +78,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
+  monsterStats.style.display = "none";
   button1.innerText = location["button text"][0];
   button2.innerText = location["button text"][1];
   button3.innerText = location["button text"][2];
@@ -192,5 +193,6 @@ function defeatMonster() {
 }
 
 function lose() {
-
+  update(locations[5]);
 }
+
