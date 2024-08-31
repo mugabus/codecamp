3b379f9976494df0db1fd6653ddc32e66a882d9c
mugabus <fatakimugaruka91@gmail.com>
2024-08-31 13:46:37 +0200

    still building the Attack function

--- finalchallenge/script.js
+++ finalchallenge/script.js
@@ -163,7 +163,12 @@ function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
   health -= monsters[fighting].level;
-
+  monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
+  if (health <= 0) {
+    lose();
+  }
 }
 
 function dodge() {
