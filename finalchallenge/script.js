c7f52c5a2b55b71ee8c235103742f720263e59d8
mugabus <fatakimugaruka91@gmail.com>
2024-08-31 21:03:40 +0200

    adding another object

--- finalchallenge/script.js
+++ finalchallenge/script.js
@@ -70,6 +70,12 @@ const locations = [
     "button functions": [goTown, goTown, goTown],
     text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
   },
+  {
+    name: "lose",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You die. &#x2620;"
+  }
 ];
 
 // initialize buttons
@@ -196,3 +202,14 @@ function lose() {
   update(locations[5]);
 }
 
+function restart() {
+  xp = 0;
+  health = 100;
+  gold = 50;
+  currentWeaponIndex = 0;
+  inventory = ["stick"];
+  goldText.innerText = gold;
+  healthText.innerText = health;
+  xpText.innerText = xp;
+  goTown();
+}
