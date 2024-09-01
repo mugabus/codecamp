ed09e14adaa05b37e6177d21dbfb0e8ac6f02887
mugabus <fatakimugaruka91@gmail.com>
2024-09-01 21:36:27 +0200

    upadting the function

--- finalchallenge/newchan/script.js
+++ finalchallenge/newchan/script.js
@@ -17,6 +17,9 @@ function isInvalidInput(str) {
 }
 
 function addEntry() {
-  const targetId = '#' + entryDropdown.value;
-   const targetInputContainer = document.querySelector(targetId + ' .input-container');
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = `
+<label>Entry ${entryNumber} Name</label>  
+  `;
 }
