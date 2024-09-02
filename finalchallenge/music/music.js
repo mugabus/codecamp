2f5620afe7eae1d73874dc330a40eb3654d25c0f
mugabus <fatakimugaruka91@gmail.com>
2024-09-02 23:30:20 +0200

    update

--- finalchallenge/music/music.js
+++ finalchallenge/music/music.js
@@ -86,7 +86,19 @@ let userData = {
 };
 
 const playSong = (id) => {
+  const song = userData?.songs.find((song) => song.id === id);
+  audio.src = song.src;
+  audio.title = song.title;
 
+  if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+    audio.currentTime = 0;
+  } else {
+    audio.currentTime = userData?.songCurrentTime;
+  }
+  userData.currentSong = song;
+  playButton.classList.add("playing");
+
+  audio.play();
 };
 
 const renderSongs = (array) => {
@@ -111,6 +123,12 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 
+playButton.addEventListener("click", () => {
+if(!userData?.currentSong){
+  playSong(userData?.songs[0].id);
+}
+});
+
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
     if (a.title < b.title) {
@@ -127,4 +145,5 @@ const sortSongs = () => {
   return userData?.songs;
 };
 
-renderSongs(sortSongs());
+
+renderSongs(userData?.songs);
