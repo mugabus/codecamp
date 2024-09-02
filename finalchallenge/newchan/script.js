f33e4c6a377907182f31f4578e8c1480593adf69
mugabus <fatakimugaruka91@gmail.com>
2024-09-02 08:45:59 +0200

    updating

--- finalchallenge/newchan/script.js
+++ finalchallenge/newchan/script.js
@@ -34,10 +34,16 @@ function addEntry() {
 
 function getCaloriesFromInputs(list) {
   let calories = 0;
-   for(const item of list){
 
-   }
+  for (const item of list) {
+    const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal);
 
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+
+    }
+  }
 }
 
 addEntryButton.addEventListener("click", addEntry);
