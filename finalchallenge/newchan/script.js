2450db809ce1d7e78cd7b0230b4100cbef1dafa6
mugabus <fatakimugaruka91@gmail.com>
2024-09-02 09:57:37 +0200

    updating

--- finalchallenge/newchan/script.js
+++ finalchallenge/newchan/script.js
@@ -52,9 +52,13 @@ function calculateCalories(e) {
   if (isError) {
     return;
   }
-  const consumedCalories=breakfastCalories+lunchCalories+dinnerCalories+snacksCalories;
 
-
+  const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+  const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+  const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+  output.innerHTML = `
+  <span class="${surplusOrDeficit.toLowerCase()}"></span>
+  `;
 }
 
 function getCaloriesFromInputs(list) {
