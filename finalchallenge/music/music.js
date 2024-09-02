96f3e1144a480dcec2527f0108a48ded90f7a66e
mugabus <fatakimugaruka91@gmail.com>
2024-09-02 22:37:35 +0200

    if statement

--- finalchallenge/music/music.js
+++ finalchallenge/music/music.js
@@ -107,4 +107,19 @@ const renderSongs = (array) => {
 
   playlistSongs.innerHTML = songsHTML;
 };
+
+const sortSongs = () => {
+  userData?.songs.sort((a,b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+
+    if (a.title > b.title) {
+      return 1;
+    }
+
+
+  });
+};
+
 renderSongs(userData?.songs);
