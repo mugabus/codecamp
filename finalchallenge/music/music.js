96e77eda62e972372f080538a5ede0fff1d6f89c
mugabus <fatakimugaruka91@gmail.com>
2024-09-03 12:07:57 +0200

    updating

--- finalchallenge/music/music.js
+++ finalchallenge/music/music.js
@@ -99,6 +99,7 @@ const playSong = (id) => {
   playButton.classList.add("playing");
 
   highlightCurrentSong();
+  
   audio.play();
 };
 
@@ -133,7 +134,11 @@ const playPreviousSong = () => {
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
   const songArtist = document.getElementById("player-song-artist");
-  
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
 };
 
 const highlightCurrentSong = () => {
