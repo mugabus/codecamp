8070d77f82df81bbd03e123ba3c272a559607bb8
mugabus <fatakimugaruka91@gmail.com>
2024-09-03 12:17:17 +0200

    updating

--- finalchallenge/music/music.js
+++ finalchallenge/music/music.js
@@ -100,6 +100,7 @@ const playSong = (id) => {
 
   highlightCurrentSong();
   setPlayerDisplay();
+  setPlayButtonAccessibleText();
   audio.play();
 };
 
@@ -130,6 +131,8 @@ const playPreviousSong = () => {
     playSong(previousSong.id);
    }
 };
+
+  
 
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
@@ -178,7 +181,11 @@ const renderSongs = (array) => {
 
 const setPlayButtonAccessibleText = () => {
   const song = userData?.currentSong || userData?.songs[0];
-  
+
+  playButton.setAttribute(
+    "aria-label",
+    song?.title ? `Play ${song.title}` : "Play"
+  );
 };
 
 const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
