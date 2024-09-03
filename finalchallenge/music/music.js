ee2f3764e0d5dc648a61177324d3fd4e369c14ed
mugabus <fatakimugaruka91@gmail.com>
2024-09-03 14:44:18 +0200

    updating

--- finalchallenge/music/music.js
+++ finalchallenge/music/music.js
@@ -159,8 +159,17 @@ const deleteSong = (id) => {
 
   if (userData?.songs.length === 0) {
     const resetButton = document.createElement("button");
-  const resetText=document.createTextNode("Reset Playlist");
+    const resetText = document.createTextNode("Reset Playlist");
+
+    resetButton.id = "reset";
+    resetButton.ariaLabel = "Reset playlist";
+    resetButton.appendChild(resetText);
+    playlistSongs.appendChild(resetButton);
+
+    resetButton.addEventListener("click", () => {
   
+    });
+
   }
 
 };
