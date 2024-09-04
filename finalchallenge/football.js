2bb398cb54d00f65e05e33d9e1a69596dae20874
mugabus <fatakimugaruka91@gmail.com>
2024-09-04 12:56:29 +0200

    setPlayerCards function

--- finalchallenge/football.js
+++ finalchallenge/football.js
@@ -175,5 +175,13 @@ Object.freeze(myFavoriteFootballTeam);
 const { sport, team, year, players } = myFavoriteFootballTeam;
 const { coachName } = myFavoriteFootballTeam.headCoach;
 
+typeOfSport.textContent = sport;
+teamName.textContent = team;
+worldCupYear.textContent = year;
+headCoach.textContent = coachName;
 
+
+const setPlayerCards = (arr=players) => {
+
+};
 
