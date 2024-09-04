d5b832d39a9adeedad79ef1fe987bf7354e55494
mugabus <fatakimugaruka91@gmail.com>
2024-09-04 11:47:12 +0200

    adding

--- finalchallenge/music/date.js
+++ finalchallenge/music/date.js
@@ -21,7 +21,8 @@ dateOptionsSelectElement.addEventListener("change", () => {
         .join("-");
       break;
     case "mm-dd-yyyy-h-mm":
-      currentDateParagraph.textContent = ``;
+      currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
       break;
+    
   }
 });
