09af1d12b58e912593c838b2f2242efa8c87b308
mugabus <fatakimugaruka91@gmail.com>
2024-09-05 22:12:28 +0200

    adding two buttons

--- finalchallenge/building-a-todo-app.js
+++ finalchallenge/building-a-todo-app.js
@@ -45,13 +45,17 @@ taskForm.addEventListener("submit", (e) => {
   }
 
   taskData.forEach(({id, title, date, description}) => {
-        tasksContainer.innerHTML += `
-          <div class="task" id="${id}">
-            <p><strong>Title:</strong> ${title}</p>
-            <p><strong>Date:</strong> ${date}</p>
-            
-          </div>
-        `
-      }
-    );
+      tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          <button type="button" class="btn">Edit</button>
+          <button type="button" class="btn">Delete</button>
+        </div>
+      `
+    }
+  );
+
 });
+
