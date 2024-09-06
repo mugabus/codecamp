39365e576680a73509dfcbb16264b4ffbef243bc
mugabus <fatakimugaruka91@gmail.com>
2024-09-06 08:52:44 +0200

    updating

--- finalchallenge/building-a-todo-app.js
+++ finalchallenge/building-a-todo-app.js
@@ -59,9 +59,15 @@ const deleteTask = (buttonEl) => {
 }
 
 const editTask = (buttonEl) => {
-  const dataArrIndex = taskData.findIndex((item) => item.id === buttonEl.parentElement.id);
-  
+    const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
 
+  currentTask = taskData[dataArrIndex];
+  
+  titleInput.value = currentTask.title;
+  dateInput.value = currentTask.date;
+  descriptionInput.value = currentTask.description;
 }
 
 const reset = () => {
