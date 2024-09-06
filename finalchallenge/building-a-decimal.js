aefdd65c9a12f64102160579acfcbefde4682dba
mugabus <fatakimugaruka91@gmail.com>
2024-09-06 15:30:32 +0200

    creation callback function

--- finalchallenge/building-a-decimal.js
+++ finalchallenge/building-a-decimal.js
@@ -3,5 +3,11 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const checkUserInput = () => {
-console.log(numberInput.value);  
+  console.log(numberInput.value);
 };
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  console.log(e);
+});
