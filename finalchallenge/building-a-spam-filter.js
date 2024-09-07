af9efc56c590569320a14b7fdeca71160fcc525d
mugabus <fatakimugaruka91@gmail.com>
2024-09-07 12:14:16 +0200

    upadting the app

--- finalchallenge/building-a-spam-filter.js
+++ finalchallenge/building-a-spam-filter.js
@@ -2,7 +2,11 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
-const isSpam = (msg) => false;
+const helpRegex = /please help|assist me/i;
+
+const denyList = [helpRegex];
+
+const isSpam = (msg) => helpRegex.test(msg);
 
 checkMessageButton.addEventListener("click", () => {
   if (messageInput.value === "") {
@@ -10,5 +14,8 @@ checkMessageButton.addEventListener("click", () => {
     return;
   }
 
-
+  result.textContent = isSpam(messageInput.value)
+    ? "Oh no! This looks like a spam message."
+    : "This message does not seem to contain any spam.";
+  messageInput.value = "";
 });
