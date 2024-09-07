f6fa41026c2f68383bfe2f892b76400b4a4dd53f
mugabus <fatakimugaruka91@gmail.com>
2024-09-07 13:45:29 +0200

    updating

--- finalchallenge/number-sorter.js
+++ finalchallenge/number-sorter.js
@@ -6,12 +6,17 @@ const sortInputArray = (event) => {
   const inputValues = [
     ...document.getElementsByClassName("values-dropdown")
   ].map((dropdown) => Number(dropdown.value));
+
+  updateUI(inputValues);
 }
 
 const updateUI = (array = []) => {
   array.forEach((num, i) => {
-
+    const outputValueNode = document.getElementById(`output-value-${i}`);
+    outputValueNode.innerText = num;
   })
 }
+
+
 
 sortButton.addEventListener("click", sortInputArray);
