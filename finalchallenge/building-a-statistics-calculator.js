abf783429da03dcaaf195c438e4c7e48b8bf5417
mugabus <fatakimugaruka91@gmail.com>
2024-09-08 09:55:55 +0200

    update the getMean function

--- finalchallenge/building-a-statistics-calculator.js
+++ finalchallenge/building-a-statistics-calculator.js
@@ -1,9 +1,9 @@
-const getMean = (array) => {
-    const sum = array.reduce((acc,el)=>(acc +el));
-  }
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const calculate = () => {
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
   
-  const calculate = () => {
-    const value = document.querySelector("#numbers").value;
-    const array = value.split(/,\s*/g);
-    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-  }
+
+}
