e4717e9609410cb09bb1fe753e552d09e569fa77
mugabus <fatakimugaruka91@gmail.com>
2024-09-08 10:44:00 +0200

    updating the function

--- finalchallenge/building-a-statistics-calculator.js
+++ finalchallenge/building-a-statistics-calculator.js
@@ -1,23 +1,22 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
-const testArr1 = [1, 2, 3, 4, 5];
-const testArr2 = [1, 2, 3, 4, 5, 6];
-const isEven = testArr2.length % 2 === 0;
-console.log(isEven);
-const oddListMedian = testArr1[Math.floor(testArr1.length / 2)];
-console.log(oddListMedian);
-const evenListMedian = getMean([testArr2[testArr2.length / 2 - 1], testArr2[testArr2.length / 2]]);
-console.log(evenListMedian);
 const getMedian = (array) => {
-  const sorted = array.sort((a, b) => a - b);
+  const sorted = array.toSorted((a, b) => a - b);
+  const median =
+    sorted.length % 2 === 0
+      ? getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 - 1]])
+      : sorted[Math.floor(sorted.length / 2)];
+  return median;
 }
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-  
+
   const mean = getMean(numbers);
 
+
   document.querySelector("#mean").textContent = mean;
+
 }
