62bb2dad419e46f25b23dd137121f03bff8550ef
mugabus <fatakimugaruka91@gmail.com>
2024-09-08 10:10:27 +0200

    creating the getMedian function

--- finalchallenge/building-a-statistics-calculator.js
+++ finalchallenge/building-a-statistics-calculator.js
@@ -1,10 +1,15 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
+const getMedian = (array) => {
+
+}
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-  const mean=getMean(numbers)
   
+  const mean = getMean(numbers);
 
+  document.querySelector("#mean").textContent = mean;
 }
