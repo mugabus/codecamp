bb771f9a40e86cdd4a0680e34c265fede47ee631
mugabus <fatakimugaruka91@gmail.com>
2024-09-08 12:28:06 +0200

    creating the getVariance function

--- finalchallenge/building-a-statistics-calculator.js
+++ finalchallenge/building-a-statistics-calculator.js
@@ -30,6 +30,10 @@ const getRange = (array) => {
   return Math.max(...array) - Math.min(...array);
 }
 
+const getVariance=(array)=>{
+   const mean=getMean(array)
+}
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
@@ -40,10 +44,8 @@ const calculate = () => {
   const mode = getMode(numbers);
   const range = getRange(numbers);
 
-
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
   document.querySelector("#mode").textContent = mode;
   document.querySelector("#range").textContent = range;
-
 }
