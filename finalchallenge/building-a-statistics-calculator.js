c298173392380202c123f16908a91e86d6c57bec
mugabus <fatakimugaruka91@gmail.com>
2024-09-08 10:48:26 +0200

    creating the getMode function

--- finalchallenge/building-a-statistics-calculator.js
+++ finalchallenge/building-a-statistics-calculator.js
@@ -9,15 +9,19 @@ const getMedian = (array) => {
   return median;
 }
 
+const getMode=(array)=>{
+  
+}
+
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-
+  
   const mean = getMean(numbers);
-  const median=getMedian(numbers);
+  const median = getMedian(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
-
 }
