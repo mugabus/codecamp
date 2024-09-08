0f95cfb2b997d358a0386262220fb6c845c570d5
mugabus <fatakimugaruka91@gmail.com>
2024-09-08 12:37:58 +0200

    updating

--- finalchallenge/building-a-statistics-calculator.js
+++ finalchallenge/building-a-statistics-calculator.js
@@ -30,8 +30,17 @@ const getRange = (array) => {
   return Math.max(...array) - Math.min(...array);
 }
 
-const getVariance=(array)=>{
-   const mean=getMean(array)
+const getVariance = (array) => {
+  const mean = getMean(array);
+  const differences = array.map(
+    el => el - mean
+  );
+  const squaredDifferences = differences.map(
+    el => el ** 2
+  );
+  const sumSquaredDifferences = squaredDifferences.reduce(
+    (acc, el) => acc + el, 0
+  );
 }
 
 const calculate = () => {
