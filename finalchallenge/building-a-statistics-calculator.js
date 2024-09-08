64788a4cb2b07c13eb78ca0466f4178437c1b357
mugabus <fatakimugaruka91@gmail.com>
2024-09-08 09:46:05 +0200

    adding the sum variable

--- finalchallenge/building-a-statistics-calculator.js
+++ finalchallenge/building-a-statistics-calculator.js
@@ -1,9 +1,9 @@
 const getMean = (array) => {
-
-}
-
-const calculate = () => {
-  const value = document.querySelector("#numbers").value;
-  const array = value.split(/,\s*/g);
-  const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-}
+    const sum = array.reduce((acc,el)=>(acc +el));
+  }
+  
+  const calculate = () => {
+    const value = document.querySelector("#numbers").value;
+    const array = value.split(/,\s*/g);
+    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+  }
