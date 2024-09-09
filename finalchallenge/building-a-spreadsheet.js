6bc90d325c7b3b6fa04eed26808d89aa2813bb29
mugabus <fatakimugaruka91@gmail.com>
2024-09-09 13:04:04 +0200

    creating the evalFormula

--- finalchallenge/building-a-spreadsheet.js
+++ finalchallenge/building-a-spreadsheet.js
@@ -20,7 +20,9 @@ const spreadsheetFunctions = {
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
 
-
+const evalFormula = (x, cells) => {
+  const idToText = id => cells.find(cell => cell.id === id);
+}
 
 window.onload = () => {
   const container = document.getElementById("container");
