c1d7de499241cd8091978db27200c14c7ee69f0d
mugabus <fatakimugaruka91@gmail.com>
2024-09-10 08:26:13 +0200

    new

--- finalchallenge/building-a-spreadsheet.js
+++ finalchallenge/building-a-spreadsheet.js
@@ -37,6 +37,7 @@ const applyFunction = str => {
   const infix = /([\d.]+)([+-])([\d.]+)/;
   const str2 = infixEval(noHigh, infix);
   const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
+  const toNumberList = args => args.split(",").map(parseFloat);
 
 }
 
