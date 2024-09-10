0d56a00f8bfe6ebb37780766578f7a53c9c83882
mugabus <fatakimugaruka91@gmail.com>
2024-09-10 08:28:25 +0200

    new

--- finalchallenge/building-a-spreadsheet.js
+++ finalchallenge/building-a-spreadsheet.js
@@ -38,7 +38,8 @@ const applyFunction = str => {
   const str2 = infixEval(noHigh, infix);
   const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
   const toNumberList = args => args.split(",").map(parseFloat);
-  const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()];
+  const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
+
 }
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
