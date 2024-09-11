e1a708e7d9e5c9c2d087a57756d3e4c4d4110958
mugabus <fatakimugaruka91@gmail.com>
2024-09-12 00:21:34 +0200

    creating the ShoppingCart function

--- finalchallenge/card-app/script.js
+++ finalchallenge/card-app/script.js
@@ -92,8 +92,19 @@ products.forEach(
         <h2>${name}</h2>
         <p class="dessert-price">$${price}</p>
         <p class="product-category">Category: ${category}</p>
-        <button id="${id}" class="btn add-to-cart-btn">Add to cart</button>
+        <button 
+          id="${id}" 
+          class="btn add-to-cart-btn">Add to cart
+        </button>
       </div>
     `;
   }
 );
+
+class ShoppingCart {
+  constructor() {
+    this.items=[];
+    this.total=0;
+    this.taxRate=8.25;
+  }
+};
