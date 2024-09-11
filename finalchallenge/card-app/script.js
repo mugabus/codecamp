6aac14844c7916403a7f21121c592144622168e5
mugabus <fatakimugaruka91@gmail.com>
2024-09-12 00:38:19 +0200

    new updating

--- finalchallenge/card-app/script.js
+++ finalchallenge/card-app/script.js
@@ -112,5 +112,10 @@ class ShoppingCart {
     const product = products.find((item) => item.id === id);
     const { name, price } = product;
     this.items.push(product);
+
+    const totalCountPerProduct = {};
+    this.items.forEach((dessert) => {
+      totalCountPerProduct[dessert.id] = totalCountPerProduct[dessert.id] + 1;
+    })
   }
 };
