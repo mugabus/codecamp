a1f1778dfa8d7a032307eb5350547f1b0b1337b4
mugabus <fatakimugaruka91@gmail.com>
2024-09-12 03:18:44 +0200

    The next step is to add the logic for increasing or decreasing a player's velocity based on if they move to the left or right of the screen.
    Inside the animate function, create an if statement where the condition checks if the right key was pressed and the player's x position is less than proportionalSize(400).
    
    You need to use the proportionalSize function here to make sure the player's x position is always proportional to the screen size.

--- finalchallenge/card-app/game/script.js
+++ finalchallenge/card-app/game/script.js
@@ -62,9 +62,24 @@ const player = new Player();
 const animate = () => {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+
+
+  if (keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
+
+  }
 
 }
 
+
+const keys = {
+  rightKey: {
+    pressed: false
+  },
+  leftKey: {
+    pressed: false
+  }
+};
 
 const startGame = () => {
   canvas.style.display = "block";
