186f9c9c311844fbaca685780be15bec7e1f8d60
mugabus <fatakimugaruka91@gmail.com>
2024-09-12 11:40:04 +0200

    In order to start working with the data, you will need to use another .then() method.
    Chain another .then() to the existing .then() method. This time, pass in data as the parameter for the callback function. For the callback, use a curly brace because you will have more than one expression. Within your callback function, log data to the console to see what it looks like.

--- finalchallenge/building-an-fcc-authors-page/script.js
+++ finalchallenge/building-an-fcc-authors-page/script.js
@@ -1,4 +1,7 @@
 const authorContainer = document.getElementById('author-container');
 const loadMoreBtn = document.getElementById('load-more-btn');
 
-fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
+fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
+  .then((res) => res.json()).then((data)=>{
+    console.log(data)
+  })
