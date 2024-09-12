760c1eaf0434ac69ca848a22db717f0b37883f58
mugabus <fatakimugaruka91@gmail.com>
2024-09-12 12:27:05 +0200

    Remember that in step 1 you selected the Load More Authors button and assigned it to loadMoreBtn.
    Use addEventListener to add a "click" event listener to loadMoreBtn. Also, pass in a reference to the fetchMoreAuthors function to run whenever the button is clicked.
    
    After that, when you click the button you should see 8 more authors

--- finalchallenge/building-an-fcc-authors-page/script.js
+++ finalchallenge/building-an-fcc-authors-page/script.js
@@ -15,9 +15,12 @@ fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
     console.error(`There was an error: ${err}`);
   });
 
-const fetchMoreAuthors=()=>{
-  
-}
+const fetchMoreAuthors = () => {
+  startingIndex += 8;
+  endingIndex += 8;
+
+  displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+};
 
 const displayAuthors = (authors) => {
   authors.forEach(({ author, image, url, bio }, index) => {
@@ -31,3 +34,5 @@ const displayAuthors = (authors) => {
   `;
   });
 };
+
+loadMoreBtn.addEventListener("click",fetchMoreAuthors)
