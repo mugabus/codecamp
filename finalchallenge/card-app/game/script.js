4c1475fc929cb8a6b6e52d8eec9a1d0cbec7158d
mugabus <fatakimugaruka91@gmail.com>
2024-09-12 03:01:41 +0200

    updating

--- finalchallenge/card-app/game/script.js
+++ finalchallenge/card-app/game/script.js
@@ -1,4 +1,3 @@
-
 const startBtn = document.getElementById("start-btn");
 const canvas = document.getElementById("canvas");
 const startScreen = document.querySelector(".start-screen");
@@ -32,7 +31,25 @@ class Player {
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
   
-update(){}
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      if (this.position.y < 0) {
+        this.position.y = 0;
+        this.velocity.y = gravity;
+      }
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+
+    if (this.position.x < this.width) {
+      this.position.x = this.width;
+    }
 
 
+  }
 }
