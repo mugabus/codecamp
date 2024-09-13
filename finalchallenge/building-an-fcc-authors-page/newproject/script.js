545135c1ec2832540009717d2ead6caebf7f462a
mugabus <fatakimugaruka91@gmail.com>
2024-09-13 10:14:15 +0200

    Inside the fourth td element, update the current value to instead call the viewCount function with the views variable as an argument.

--- finalchallenge/building-an-fcc-authors-page/newproject/script.js
+++ finalchallenge/building-an-fcc-authors-page/newproject/script.js
@@ -72,7 +72,7 @@ const showLatestPosts = (data) => {
       </td>
       <td></td>
       <td>${posts_count - 1}</td>
-      <td>${views}</td>
+      <td>${viewCount(views)}</td>
       <td>${timeAgo(bumped_at)}</td>
     </tr>`;
   }).join("");
