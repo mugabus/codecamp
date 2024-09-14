131eca417cc16d60a3a241b286620b41cc549e97
mugabus <fatakimugaruka91@gmail.com>
2024-09-15 00:40:28 +0200

    mew update

--- finalchallenge/building-an-fcc-authors-page/bootstrap/react/index.jsx
+++ finalchallenge/building-an-fcc-authors-page/bootstrap/react/index.jsx
@@ -1,23 +1,22 @@
-const CurrentDate = (props) => {
-    return (
-      <div>
-        { /* Change code below this line */ }
-        <p>The current date is: {props.date}</p>
-        { /* Change code above this line */ }
-      </div>
-    );
+const List = (props) => {
+    { /* Change code below this line */ }
+    return <p>{props.tasks.join(", ")}</p>;
+    { /* Change code above this line */ }
   };
   
-  class Calendar extends React.Component {
+  class ToDo extends React.Component {
     constructor(props) {
       super(props);
     }
     render() {
       return (
         <div>
-          <h3>What date is it?</h3>
+          <h1>To Do Lists</h1>
+          <h2>Today</h2>
           { /* Change code below this line */ }
-          <CurrentDate date={Date()} />
+          <List tasks={["walk dog", "workout"]} />
+          <h2>Tomorrow</h2>
+          <List tasks={["buy groceries", "study React", "call mom"]} />
           { /* Change code above this line */ }
         </div>
       );
