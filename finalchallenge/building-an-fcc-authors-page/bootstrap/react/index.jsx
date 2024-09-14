e02648ce9fd9b759ade9e3c6fae44839f9670381
mugabus <fatakimugaruka91@gmail.com>
2024-09-15 00:32:45 +0200

    new update

--- finalchallenge/building-an-fcc-authors-page/bootstrap/react/index.jsx
+++ finalchallenge/building-an-fcc-authors-page/bootstrap/react/index.jsx
@@ -1,16 +1,26 @@
-class MyComponent extends React.Component {
+const CurrentDate = (props) => {
+    return (
+      <div>
+        { /* Change code below this line */ }
+        <p>The current date is: {props.date}</p>
+        { /* Change code above this line */ }
+      </div>
+    );
+  };
+  
+  class Calendar extends React.Component {
     constructor(props) {
       super(props);
     }
-  
     render() {
       return (
         <div>
-          <h1>My First React Component!</h1>
+          <h3>What date is it?</h3>
+          { /* Change code below this line */ }
+          <CurrentDate date={Date()} />
+          { /* Change code above this line */ }
         </div>
       );
     }
-  }
-  
-  ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'));
+  };
   
