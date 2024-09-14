1b38578f3212ec789d764c5eef8d42e227bbcbd5
mugabus <fatakimugaruka91@gmail.com>
2024-09-15 00:23:58 +0200

    update

--- finalchallenge/building-an-fcc-authors-page/bootstrap/react/index.jsx
+++ finalchallenge/building-an-fcc-authors-page/bootstrap/react/index.jsx
@@ -1,20 +1,16 @@
-class TypesOfFood extends React.Component {
+class MyComponent extends React.Component {
     constructor(props) {
       super(props);
     }
+  
     render() {
       return (
         <div>
-          <h1>Types of Food:</h1>
-          {/* Change code below this line */}
-          <Fruits />
-          <Vegetables />
-          {/* Change code above this line */}
+          <h1>My First React Component!</h1>
         </div>
       );
     }
   }
   
-  // Change code below this line
-  ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'));
+  ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'));
   
