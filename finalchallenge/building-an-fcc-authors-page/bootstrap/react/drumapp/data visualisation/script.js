c1b241450d0e455e182aed3077badf3f5c342fb2
mugabus <fatakimugaruka91@gmail.com>
2024-09-16 09:42:51 +0200

    updating

--- finalchallenge/building-an-fcc-authors-page/bootstrap/react/drumapp/data visualisation/script.js
+++ finalchallenge/building-an-fcc-authors-page/bootstrap/react/drumapp/data visualisation/script.js
@@ -2,10 +2,12 @@
   <script>
     const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
 
- d3.select("body").selectAll("h2")
+    d3.select("body").selectAll("h2")
       .data(dataset)
       .enter()
       .append("h2")
-      .text("New Title");
+      // Add your code below this line
+      .text((d) => d + " USD");
+      // Add your code above this line
   </script>
 </body>
